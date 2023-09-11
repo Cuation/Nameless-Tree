18a6885c1a3a53c7005beea1ade197876e05cc35
Cuation <cautiongx@gmail.com>
2023-09-11 16:36:00 -0400

    Update themes.js

--- js/utils/themes.js
+++ js/utils/themes.js
@@ -48,7 +48,7 @@ function getThemeName() {
 
 function switchTheme() {
 	let index = themes.indexOf(options.theme)
-	if (options.theme === null || index >= themes.length-1 || index < 1) {
+	if (options.theme === null || index >= themes.length-1 || index < -1) {
 		options.theme = themes[0];
 	}
 	else {
