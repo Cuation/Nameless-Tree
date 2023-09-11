3b16e7d47120c7f34855479ec3717f1e34d62262
Cuation <cautiongx@gmail.com>
2023-09-11 16:37:29 -0400

    Update themes.js

--- js/utils/themes.js
+++ js/utils/themes.js
@@ -48,14 +48,13 @@ function getThemeName() {
 
 function switchTheme() {
 	let index = themes.indexOf(options.theme)
-	if (options.theme === null || index >= themes.length-1 || index < -1) {
+	if (options.theme === null || index >= themes.length-1 || index < 1) {
 		options.theme = themes[0];
 	}
 	else {
 		index ++;
 		options.theme = themes[index];
 		options.theme = themes[1];
-		options.theme = themes[2];
 	}
 	changeTheme();
 	resizeCanvas();
